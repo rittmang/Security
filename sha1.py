3f61ecd400023c11ab84e6a5c9d50e761305c8dc
rittmang <ritomgupta99@gmail.com>
2019-11-19 14:52:54 +0530

    Add file output
    Signed-off-by: rittmang <ritomgupta99@gmail.com>

--- sha1.py
+++ sha1.py
@@ -5,4 +5,8 @@ str=f.read()
 result=hashlib.sha1(str.encode())
 
 print("The hec equivalent of SHA1 is: ",result.hexdigest())
+g=open("output.txt","w+")
+g.write(result.hexdigest())
+g.close()
+f.close()
 print("\r")
